a51b525683a4c2f356f057927f7dcb6a35422644
alpeshweta <alpeshweta@gmail.com>
2026-04-09 06:46:17 +1000

    feat: add /fund/tdp/:productId route for TdpDashboardPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 import { DataProvider } from './context/PerformanceDataContext'
 import { SearchPage } from './pages/SearchPage'
 import { DashboardPage } from './pages/DashboardPage'
+import { TdpDashboardPage } from './pages/TdpDashboardPage'
 
 export function App() {
   return (
@@ -10,6 +11,7 @@ export function App() {
         <Routes>
           <Route path="/" element={<SearchPage />} />
           <Route path="/fund/:productId" element={<DashboardPage />} />
+          <Route path="/fund/tdp/:productId" element={<TdpDashboardPage />} />
         </Routes>
       </HashRouter>
     </DataProvider>
