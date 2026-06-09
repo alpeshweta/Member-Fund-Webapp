4ef6d8e9a24668fe23775bd87c3200392991653b
Shweta Shah <alpeshweta@gmail.com>
2026-06-09 15:44:32 +1000

    feat: wire DisclaimerModal and DisclaimerBanner into App

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { DataProvider } from './context/PerformanceDataContext'
 import { SearchPage } from './pages/SearchPage'
 import { DashboardPage } from './pages/DashboardPage'
 import { TdpDashboardPage } from './pages/TdpDashboardPage'
+import { DisclaimerModal } from './components/DisclaimerModal'
+import { DisclaimerBanner } from './components/DisclaimerBanner'
 
 export function App() {
-  return (
-    <DataProvider>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<SearchPage />} />
-          <Route path="/fund/:productId" element={<DashboardPage />} />
-          <Route path="/fund/tdp/:productId" element={<TdpDashboardPage />} />
-        </Routes>
-      </HashRouter>
-    </DataProvider>
-  )
+    return (
+          <DataProvider>
+                <DisclaimerModal />
+                <DisclaimerBanner />
+                <HashRouter>
+                        <Routes>
+                                  <Route path="/" element={<SearchPage />} />
+                                  <Route path="/fund/:productId" element={<DashboardPage />} />
+                                  <Route path="/fund/tdp/:productId" element={<TdpDashboardPage />} />
+                        </Routes>
+                </HashRouter>
+          </DataProvider>
+        )
 }
